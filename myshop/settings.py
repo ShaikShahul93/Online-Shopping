625fe36b8df2df94a30aeb94206ab02136f2f423
shahul <shaikshahul1316@gmail.com>
2026-06-21 08:45:57 +0530

    Fix deployment issues for Render

--- myshop/settings.py
+++ myshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u5_s%&=m_7n!&4#=4tfie#6!j+my1mh0sf_3+!x6116497b9!m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
 
 
 # Default primary key field type
